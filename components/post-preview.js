c348468de73c39d1821a8286a20460a6bf24a74e
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 09:19:05 +0100

    ergreg

--- components/post-preview.js
+++ components/post-preview.js
@@ -18,7 +18,7 @@ export default function PostPreview({
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`}>
-          <a className="hover:underline">{title}</a>
+          <a className="itemTitle">{title}</a>
         </Link>
       </h3>
       <div className="text-lg mb-4">
