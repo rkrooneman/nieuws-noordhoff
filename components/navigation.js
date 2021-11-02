fe8a9f144c69fd5eb391d5bdef0adb6c72a4b38a
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 17:09:03 +0100

    rthrth

--- components/navigation.js
+++ components/navigation.js
@@ -7,5 +7,15 @@ export default function Navigation() {
             <a className="navLink">Middelbaar Beroepsonderwijs</a>
             <a className="navLink">Hoger Onderwijs</a>
             <a className="navLink groupEnd">Professional</a>
+            <span className="navGroup font-bold groupStart">Categorieën</span>
+            <a className="navLink">Webinars</a>
+            <a className="navLink">Lestips</a>
+            <a className="navLink">Actualiteit</a>
+            <a className="navLink groupEnd">Vernieuwing</a>
+            <span className="navGroup font-bold groupStart">Doelgroep</span>
+            <a className="navLink">Docenten</a>
+            <a className="navLink">Ouders</a>
+            <a className="navLink">Studenten</a>
+            <a className="navLink groupEnd">Overige</a>
         </nav>
 }
