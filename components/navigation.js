89dcfc3cfabefbbac5aeb20b3886547ed698a285
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 18:47:43 +0100

    hher

--- components/navigation.js
+++ components/navigation.js
@@ -1,20 +1,20 @@
 export default function Navigation() {
   return <nav className="navigation mx-auto px-5">
             <a className="navLink groupStart groupEnd">Noordhoff Nieuws</a>
-            <span className="navGroup font-bold groupStart">Marktgroepen</span>
+            <span className="navGroup text-x1 font-bold groupStart">Marktgroepen</span>
             <hr className="navSeparator" /><a className="navLink">Basisonderwijs</a>
             <hr className="navSeparator" /><a className="navLink">Voortgezet Onderwijs</a>
             <hr className="navSeparator" /><a className="navLink">Middelbaar Beroepsonderwijs</a>
             <hr className="navSeparator" /><a className="navLink">Hoger Onderwijs</a>
             <hr className="navSeparator" /><a className="navLink groupEnd">Professional</a>
             <hr className="navSeparator" />
-            <span className="navGroup font-bold groupStart">Categorieën</span>
+            <span className="navGroup text-x1 font-bold groupStart">Categorieën</span>
             <hr className="navSeparator" /><a className="navLink">Webinars</a>
             <hr className="navSeparator" /><a className="navLink">Lestips</a>
             <hr className="navSeparator" /><a className="navLink">Actualiteit</a>
             <hr className="navSeparator" /><a className="navLink groupEnd">Vernieuwing</a>
             <hr className="navSeparator" />
-            <span className="navGroup font-bold groupStart">Doelgroep</span>
+            <span className="navGroup text-x1 font-bold groupStart">Doelgroep</span>
             <hr className="navSeparator" /><a className="navLink">Docenten</a>
             <hr className="navSeparator" /><a className="navLink">Ouders</a>
             <hr className="navSeparator" /><a className="navLink">Studenten</a>
