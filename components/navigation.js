759a4e08bf046a02c5501ccfcb70244ec6a0b94e
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 14:56:47 +0100

    ewfewf

--- components/navigation.js
+++ components/navigation.js
@@ -1,8 +1,6 @@
 export default function Navigation() {
   return <nav className="navigation mx-auto px-5">
-            <ul>
-                <li><a className="navLink">Noordhoff Nieuws</a></li>
-            </u>
+            <a className="navLink">Noordhoff Nieuws</a>
             <a className="navLink">Basisonderwijs</a>
             <a className="navLink">Voortgezet Onderwijs</a>
             <a className="navLink">Middelbaar Beroepsonderwijs</a>
