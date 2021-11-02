76848eb094b4ea0ae9030a41413e47e77f4ecedc
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 18:44:25 +0100

    wefwef

--- components/navigation.js
+++ components/navigation.js
@@ -2,20 +2,23 @@ export default function Navigation() {
   return <nav className="navigation mx-auto px-5">
             <a className="navLink groupStart groupEnd">Noordhoff Nieuws</a>
             <span className="navGroup font-bold groupStart">Marktgroepen</span>
-            <a className="navLink">Basisonderwijs</a>
-            <a className="navLink">Voortgezet Onderwijs</a>
-            <a className="navLink">Middelbaar Beroepsonderwijs</a>
-            <a className="navLink">Hoger Onderwijs</a>
-            <a className="navLink groupEnd">Professional</a>
+            <hr className="navSeparator" /><a className="navLink">Basisonderwijs</a>
+            <hr className="navSeparator" /><a className="navLink">Voortgezet Onderwijs</a>
+            <hr className="navSeparator" /><a className="navLink">Middelbaar Beroepsonderwijs</a>
+            <hr className="navSeparator" /><a className="navLink">Hoger Onderwijs</a>
+            <hr className="navSeparator" /><a className="navLink groupEnd">Professional</a>
+            <hr className="navSeparator" />
             <span className="navGroup font-bold groupStart">Categorieën</span>
-            <a className="navLink">Webinars</a>
-            <a className="navLink">Lestips</a>
-            <a className="navLink">Actualiteit</a>
-            <a className="navLink groupEnd">Vernieuwing</a>
+            <hr className="navSeparator" /><a className="navLink">Webinars</a>
+            <hr className="navSeparator" /><a className="navLink">Lestips</a>
+            <hr className="navSeparator" /><a className="navLink">Actualiteit</a>
+            <hr className="navSeparator" /><a className="navLink groupEnd">Vernieuwing</a>
+            <hr className="navSeparator" />
             <span className="navGroup font-bold groupStart">Doelgroep</span>
-            <a className="navLink">Docenten</a>
-            <a className="navLink">Ouders</a>
-            <a className="navLink">Studenten</a>
-            <a className="navLink groupEnd">Overige</a>
+            <hr className="navSeparator" /><a className="navLink">Docenten</a>
+            <hr className="navSeparator" /><a className="navLink">Ouders</a>
+            <hr className="navSeparator" /><a className="navLink">Studenten</a>
+            <hr className="navSeparator" /><a className="navLink groupEnd">Overige</a>
+            <hr className="navSeparator" />
         </nav>
 }
