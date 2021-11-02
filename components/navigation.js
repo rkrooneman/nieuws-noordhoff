eb007e3bc08c8be5c6d26327320e7553d1600de6
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 14:43:13 +0100

    regerg

--- components/navigation.js
+++ components/navigation.js
@@ -1,5 +1,10 @@
 export default function Navigation() {
   return <nav className="navigation mx-auto px-5">
             <a className="navLink">Noordhoff Nieuws</a>
+            <a className="navLink">Basisonderwijs</a>
+            <a className="navLink">Voortgezet Onderwijs</a>
+            <a className="navLink">Middelbaar Beroepsonderwijs</a>
+            <a className="navLink">Hoger Onderwijs</a>
+            <a className="navLink">Professional</a>
         </nav>
 }
