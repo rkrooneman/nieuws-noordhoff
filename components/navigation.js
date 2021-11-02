a324c238a70f49ebc4dffe9505bf7b5f179874e6
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 10:30:35 +0100

    ewf

--- components/navigation.js
+++ components/navigation.js
@@ -1,5 +1,9 @@
 export default function Navigation() {
-  return <nav className="navigation mx-auto px-5">
-      
+  return 
+      <nav className="navigation mx-auto px-5">
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
       </nav>
 }
