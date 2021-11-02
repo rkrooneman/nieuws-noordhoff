730bc45f763715aeda96438538b8b7718fac705f
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 17:02:44 +0100

    erger

--- components/navigation.js
+++ components/navigation.js
@@ -1,7 +1,8 @@
 export default function Navigation() {
   return <nav className="navigation mx-auto px-5">
             <a className="navLink groupStart groupEnd">Noordhoff Nieuws</a>
-            <a className="navLink groupStart">Basisonderwijs</a>
+            <span className="navGroup font-bold groupStart">Marktgroepen</span>
+            <a className="navLink">Basisonderwijs</a>
             <a className="navLink">Voortgezet Onderwijs</a>
             <a className="navLink">Middelbaar Beroepsonderwijs</a>
             <a className="navLink">Hoger Onderwijs</a>
