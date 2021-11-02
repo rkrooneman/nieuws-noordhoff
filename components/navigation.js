bbb44b0be03e5c414891bc8a416e7fc2a3148a70
Roderik Krooneman <38614585+rkrooneman@users.noreply.github.com>
2021-11-02 10:38:31 +0100

    ewfe

--- components/navigation.js
+++ components/navigation.js
@@ -1,9 +1,5 @@
-import Link from 'next/link'
-
 export default function Navigation() {
   return <nav className="navigation mx-auto px-5">
-            <Link href="/">
-                <a className="navLink">Noordhoff Nieuws</a>
-            </Link>
+            <a className="navLink">Noordhoff Nieuws</a>
         </nav>
 }
